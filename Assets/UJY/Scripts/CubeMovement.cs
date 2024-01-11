6399517df86c2599d05b1ea91c9898a7a7687ad1
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2024-01-11 10:48:31 +0900

    [Add] UJYScene
    개인 씬 추가

---
+++ Assets/UJY/Scripts/CubeMovement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    private int m_delaytime;
+    private int m_movex;
+    private int m_movez;
+}
