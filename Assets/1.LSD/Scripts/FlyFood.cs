09c4db224febb67603994b759b0051610340a9f3
newb <tydeo6029@naver.com>
2024-01-11 22:17:37 +0900

    [Change] Food Game Scripts
    3개 스크립트 주석 추가
    FlyFood 스크립트 AddTorque 함수 적용

--- Assets/1.LSD/Scripts/FlyFood.cs
+++ Assets/1.LSD/Scripts/FlyFood.cs
@@ -1,39 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
 using UnityEngine;
 
 public class FlyFood : MonoBehaviour
 {
-    float xValue;
-    Rigidbody rb;
-    [SerializeField] int power;
-    [SerializeField] int jumpPower;
+    private float xValue;
+    private Rigidbody rb;
+    [SerializeField] private int power;
+    [SerializeField] private int jumpPower;
 
     void Awake()
     {
-        xValue = transform.position.x; // 생성시 푸드 x 값 받아오기
-        rb = GetComponent<Rigidbody>(); // AddForce 함수를 위한 rb
+        xValue = transform.position.x; // 처음 생성된 위치 x 값 받아오기
+        rb = GetComponent<Rigidbody>(); // AddForce, AddTorque 함수를 위한 rb
     }
     void Start()
     {
-        FlyForce();
+        FlyForce(); // 한번만 실행
         Invoke("DestroyFood", 2.5f);
     }
     void FlyForce()
     {
+        // powerRnd = power의 -2 ~ +2 값으로 변경
         int powerRnd = Random.Range(power - 2, power + 2);
+
+        // jumpPowerRnd = jumpPower의 -2 ~ +2 값으로 변경
         int jumpPowerRnd = Random.Range(jumpPower - 2, jumpPower + 2);
-        if (xValue > 0)
+
+        if (xValue > 0) // xValue 값이 0보다 클 경우 오른쪽에서 왼쪽으로 발사
         {
             rb.AddForce(-powerRnd, jumpPowerRnd, 0, ForceMode.Impulse);
-            // Torque
+            rb.AddTorque(RotationPowerRnd(), RotationPowerRnd(), RotationPowerRnd());
         }
-        else if (xValue < 0)
+        
+        else if (xValue < 0) // xValue 값이 0보다 작을 경우 왼쪽에서 오른쪽으로 발사
         {
             rb.AddForce(powerRnd, jumpPowerRnd, 0, ForceMode.Impulse);
+            rb.AddTorque(RotationPowerRnd(), RotationPowerRnd(), RotationPowerRnd());
         }
     }
+
+    int RotationPowerRnd() // Food -1000부터 1000까지 회전값 랜덤
+    {
+        int rotationPowerRnd = Random.Range(-1000, 1001);
+        return rotationPowerRnd;
+    }
+
+    // Invoke("DestroyFood", 2.5f); 함수에 필요한 함수, 2.5초 이후 오브젝트 삭제
     void DestroyFood()
     {
         Destroy(gameObject);
--- Assets/1.LSD/Scripts/MakeFood.cs
+++ Assets/1.LSD/Scripts/MakeFood.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MakeFood : MonoBehaviour
@@ -8,43 +6,39 @@ public class MakeFood : MonoBehaviour
     [SerializeField] private GameObject Banana;
     [SerializeField] private GameObject Cheese;
     [SerializeField] private GameObject Cherry;
-    [SerializeField] private int repetition;
+    [SerializeField] private int repetition; // 몇번 반복할지
 
-    // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(MyCoroutine());
+        StartCoroutine(FoodMakeCoroutine()); // 코루틴 실행
     }
-   
-    // 코루틴 n번 반복
-    IEnumerator MyCoroutine()
+
+    IEnumerator FoodMakeCoroutine() // 코루틴 repetition 값 만큼 반복
     {
         while (repetition != 0)
         {
-            FoodContainerPosition();
-            MakeFoods();
-            repetition--;
-            yield return new WaitForSeconds(.5f);
+            FoodContainerPosition(); // 포지션을 바꾸고
+            MakeFoods(); // 푸드를 만든다.
+            repetition--; // 1번 동작할때마다 repetition 값을 -
+            yield return new WaitForSeconds(.5f); // 0.5초에 한번씩
         }
     }
 
-    // 푸드 컨테이너 위치값 랜덤 설정
-    void FoodContainerPosition()
+    void FoodContainerPosition() // 푸드 컨테이너 위치값 랜덤 설정
     {
-        float rndY = Random.Range(1.6f, -6.5f);
-        int rndX = Random.Range(0, 2);
-        if (rndX == 0)
+        float rndY = Random.Range(1f, -6.5f); // Y값 위치를 만들기 위한 랜덤
+        int rnd = Random.Range(0, 2); // 50%를 만들기 위한 랜덤
+        if (rnd == 0)
         {
-            transform.position = new Vector3(-5, rndY, 4);
+            transform.position = new Vector3(-6, rndY, 0); // rnd 가 0일시 해당 좌표로 이동
         }
         else
         {
-            transform.position = new Vector3(5, rndY, 4);
+            transform.position = new Vector3(6, rndY, 0);// rnd 가 1일시 해당 좌표로 이동
         }
     }
 
-    // 푸드 생성
-    void MakeFoods()
+    void MakeFoods() // 랜덤 푸드 생성
     {
         int rnd = Random.Range(0, 3);
         switch (rnd)
