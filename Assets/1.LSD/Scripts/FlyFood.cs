c56402fb10e7e13591150614826e0e9d7cac5f1a
newb <tydeo6029@naver.com>
2024-01-12 15:27:39 +0900

    [Add] MiniGame - FoodShoot
    오브젝트 하나를 미니 게임 프리팹화
    미니 게임 프리팹 포지션이 변경되어도 변경된 위치에서 음식을 생성

--- Assets/1.LSD/Scripts/FlyFood.cs
+++ Assets/1.LSD/Scripts/FlyFood.cs
@@ -6,9 +6,11 @@ public class FlyFood : MonoBehaviour
     private Rigidbody rb;
     [SerializeField] private int power;
     [SerializeField] private int jumpPower;
+    private Transform trn;
 
     void Awake()
     {
+        trn = transform.parent.transform.parent;
         xValue = transform.position.x; // 처음 생성된 위치 x 값 받아오기
         rb = GetComponent<Rigidbody>(); // AddForce, AddTorque 함수를 위한 rb
     }
@@ -25,13 +27,13 @@ public class FlyFood : MonoBehaviour
         // jumpPowerRnd = jumpPower의 -2 ~ +2 값으로 변경
         int jumpPowerRnd = Random.Range(jumpPower - 2, jumpPower + 2);
 
-        if (xValue > 0) // xValue 값이 0보다 클 경우 오른쪽에서 왼쪽으로 발사
+        if (xValue > trn.position.x) // xValue 값이 0보다 클 경우 오른쪽에서 왼쪽으로 발사
         {
             rb.AddForce(-powerRnd, jumpPowerRnd, 0, ForceMode.Impulse);
             rb.AddTorque(RotationPowerRnd(), RotationPowerRnd(), RotationPowerRnd());
         }
         
-        else if (xValue < 0) // xValue 값이 0보다 작을 경우 왼쪽에서 오른쪽으로 발사
+        else if (xValue < trn.position.x) // xValue 값이 0보다 작을 경우 왼쪽에서 오른쪽으로 발사
         {
             rb.AddForce(powerRnd, jumpPowerRnd, 0, ForceMode.Impulse);
             rb.AddTorque(RotationPowerRnd(), RotationPowerRnd(), RotationPowerRnd());
--- Assets/1.LSD/Scripts/MakeFood.cs
+++ Assets/1.LSD/Scripts/MakeFood.cs
@@ -7,6 +7,12 @@ public class MakeFood : MonoBehaviour
     [SerializeField] private GameObject Cheese;
     [SerializeField] private GameObject Cherry;
     [SerializeField] private int repetition; // 몇번 반복할지
+    public Transform parentTrn;
+
+    void Awake()
+    {
+        parentTrn = transform.parent;
+    }
 
     void Start()
     {
@@ -17,8 +23,7 @@ public class MakeFood : MonoBehaviour
     {
         while (repetition != 0)
         {
-            FoodContainerPosition(); // 포지션을 바꾸고
-            MakeFoods(); // 푸드를 만든다.
+            FoodContainerPosition(); // 포지션을 바꾼다.
             repetition--; // 1번 동작할때마다 repetition 값을 -
             yield return new WaitForSeconds(.5f); // 0.5초에 한번씩
         }
@@ -28,29 +33,31 @@ public class MakeFood : MonoBehaviour
     {
         float rndY = Random.Range(1f, -6.5f); // Y값 위치를 만들기 위한 랜덤
         int rnd = Random.Range(0, 2); // 50%를 만들기 위한 랜덤
+        Vector3 position;
         if (rnd == 0)
         {
-            transform.position = new Vector3(-6, rndY, 0); // rnd 가 0일시 해당 좌표로 이동
+            position = new Vector3(-6 + parentTrn.position.x, rndY + parentTrn.position.y, parentTrn.position.z); // rnd 가 0일시 해당 좌표로 이동
         }
         else
         {
-            transform.position = new Vector3(6, rndY, 0);// rnd 가 1일시 해당 좌표로 이동
+            position = new Vector3(6 + parentTrn.position.x, rndY + parentTrn.position.y, parentTrn.position.z);// rnd 가 1일시 해당 좌표로 이동
         }
+        MakeFoods(position); // 푸드를 만든다.
     }
 
-    void MakeFoods() // 랜덤 푸드 생성
+    void MakeFoods(Vector3 position) // 랜덤 푸드 생성
     {
         int rnd = Random.Range(0, 3);
         switch (rnd)
         {
             case 0:
-                Instantiate(Banana, transform.position, Quaternion.identity);
+                Instantiate(Banana, position, Quaternion.identity, transform);
                 break;
             case 1:
-                Instantiate(Cheese, transform.position, Quaternion.identity);
+                Instantiate(Cheese, position, Quaternion.identity, transform);
                 break;
             case 2:
-                Instantiate(Cherry, transform.position, Quaternion.identity);
+                Instantiate(Cherry, position, Quaternion.identity, transform);
                 break;
         }
     }
