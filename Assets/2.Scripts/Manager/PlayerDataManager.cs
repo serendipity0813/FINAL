56945f35b3d293ed529f39d08fcc69d0d5fc5788
newb <tydeo6029@naver.com>
2024-01-16 16:11:03 +0900

    [Change] PlayerDataManager Code Change
    코드 리팩토링
    json save 경로 문제 해결
    불필요한 변수 반복 선언 줄이기
    게임 종료시 saveJson() 함수 호출
    haveGamesIndex, rankingPoint List로 변경

--- Assets/2.Scripts/Manager/PlayerDataManager.cs
+++ Assets/2.Scripts/Manager/PlayerDataManager.cs
@@ -1,14 +1,16 @@
-using System;
 using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDataManager : MonoBehaviour
 {
     public static PlayerDataManager instance {get; private set;}
     public PlayerData m_playerData;
+    private string path;
 
     private void Awake()
     {
+        // 싱글톤
         if (instance == null)
         {
             instance = this;
@@ -19,22 +21,23 @@ public class PlayerDataManager : MonoBehaviour
             if(instance != this)
                 Destroy(gameObject);
         }
+        // Json 데이터 위치 불러오기
+        path = Path.Combine(Application.dataPath, "Data", "playerData.json");
     }
 
     private void Start()
     {
-        if (File.Exists("playerData.json") == false)
+        if (File.Exists(path))
         {
-            //파일이 없을 경우 파일 생성 및 초기화
-            SaveJson();
-            FirstSave();
+            // json 파일이 있다면 시작할 때 json 파일 데이터 불러오기
+            LoadJson();
         }
         else
         {
-            // 게임을 처음 시작할 때 Json 파일 데이터 불러오기
-            LoadJson();
+            //파일이 없을 경우 파일 생성 및 초기화
+            FirstSave(); // PlayerData를 FirstSave()에 설정되어 있는 값으로 초기화
+            SaveJson(); // 이후 json에 저장
         }
-        SaveJson(); // 초기화를 위해 Json 파일 세이브 (필요 없어도 됨)
     }
     /* 
         다른 곳에서 활용 예시 : 게임이 끝나고 결과창
@@ -54,30 +57,22 @@ public class PlayerDataManager : MonoBehaviour
             m_playerData.level++;
         }
         ★ PlayerDataManager.instance.SaveJson(); 이후 로직 마지막에 Json 세이브
-        ★ PlayerDataManager.instance.LoadJson(); 초기화를 위한 Json 로드 (필요 없어도 됨)
+        ★ PlayerDataManager.instance.LoadJson(); 안전한 초기화를 위해 Json 다시 불러오기 (필요 없어도 됌)
         게임 결과창 표시 로직
     */
 
     // 현재 Json 파일을 저장하고 싶다면 아래 메소드를 호출
     void SaveJson()
     {   
-        // JSON 형태로 포멧팅
-        string jsonData = JsonUtility.ToJson(m_playerData,true);
-        // 데이터를 저장할 경로
-        string path = Path.Combine(Application.dataPath, "Data", "playerData.json");
-        // 파일 생성 및 저장
-        File.WriteAllText(path, jsonData);
+        string jsonData = JsonUtility.ToJson(m_playerData,true); // JSON 형태로 포멧팅
+        File.WriteAllText(path, jsonData); // 파일 생성 및 저장
     }
 
     // 저장 되어있는 Json 파일을 불러오고 싶다면 아래 메소드를 호출
     void LoadJson()
     {
-        // 데이터를 불러올 경로 지정
-        string path = Path.Combine(Application.dataPath, "Data", "playerData.json");
-        // 파일의 텍스트를 string으로 저장
-        string jsonData = File.ReadAllText(path);
-        // 이 Json데이터를 역직렬화하여 playerData에 넣어줌
-        m_playerData = JsonUtility.FromJson<PlayerData>(jsonData);
+        string jsonData = File.ReadAllText(path); // 파일의 텍스트를 string으로 저장
+        m_playerData = JsonUtility.FromJson<PlayerData>(jsonData); // 이 Json데이터를 역직렬화하여 playerData에 넣어줌
     }
 
     // 세이브 파일이 없을 시 초기화 설정 값
@@ -87,24 +82,29 @@ public class PlayerDataManager : MonoBehaviour
         m_playerData.name = "name";
         m_playerData.level = 1;
         m_playerData.exe = 0;
-        m_playerData.coin = 0;
-        m_playerData.haveGamesIndex = new bool[3] {true,true,true};
-        m_playerData.rankingPoint = new int[3] {0,0,0};
+        m_playerData.coin = 100;
+        m_playerData.haveGamesIndex = new List<bool>() {true,true,true};
+        m_playerData.rankingPoint = new List<int>() {0,0,0};
+    }
+    private void OnApplicationQuit()
+    {
+        // 게임 종료 시 실행할 작업
+        SaveJson();
     }
 }
 
-[Serializable]
+[System.Serializable]
 public class PlayerData // Json으로 파일을 Load 하거나 Save 할 때의 데이터 
 {
     // public int id; 고유 id 코드를 불러오는 것인데 아직 필요한지 모르겠음
     public string name; // 플레이어 이름
-    public int level; // 플레이어 현재 레벨
-    public float exe; // 플레이어 현재 경험치 량
-    public int coin; // 플레이어가 가지고 있는 코인 재화
+    public int level;   // 플레이어 현재 레벨
+    public float exe;   // 플레이어 현재 경험치 량
+    public int coin;    // 플레이어가 가지고 있는 코인 재화
 
     // 미니게임을 가지고 있는지 없는지 판단 false은 없고 true은 가지고 있는걸로
-    public bool[] haveGamesIndex;
+    public List<bool> haveGamesIndex;
 
     // haveGamesIndex와 인덱스가 동일하게, 점수를 기록
-    public int[] rankingPoint;
+    public List<int> rankingPoint;
 }
