e7a69e31f6c5fef6a538b833682775436211817b
WooJin <106531904+h117562@users.noreply.github.com>
2024-01-22 21:11:32 +0900

    [Change] WallCollision.cs Edit

--- Assets/2.Scripts/WallCollisions.cs
+++ Assets/2.Scripts/WallCollisions.cs
@@ -8,17 +8,17 @@ class WallCollisions : MonoBehaviour
     public GameObject m_left;
     public GameObject m_right;
 
-    [SerializeField] private float m_topSpace;
-    [SerializeField] private float m_botSpace;
-    [SerializeField] private float m_leftSpace;
-    [SerializeField] private float m_rightSpace;
+    private float m_topSpace;//화면 위쪽 남는 공간 크기
 
     private Camera m_camera;
-    private float m_camSize = 10.0f;
+    private float m_camSize = 10.0f;//현재 카메라의 사이즈
 
     private void Start()
     {
         m_camera = CameraManager.Instance.GetCamera();
+        m_camSize = m_camera.orthographicSize;
+        m_topSpace = Screen.safeArea.height / Screen.height;//기본 스크린 높이는 int형 주의
+
         SetWalls();
     }
 
@@ -30,13 +30,12 @@ class WallCollisions : MonoBehaviour
     //사방면의 벽을 화면 크기에 맞춰서 옮기는 함수
     public void SetWalls()
     {
-        float cameraHeight = 2.0f * m_camSize; // 카메라 세로 크기
-        float cameraWidth = cameraHeight * m_camera.aspect; // 카메라 가로 크기
+        float cameraWidth = m_camSize * m_camera.aspect; // 카메라 가로 크기
 
-        m_left.transform.position = new Vector3(-cameraWidth / 2.0f + m_leftSpace, 0.5f, 0.0f);
-        m_right.transform.position = new Vector3(cameraWidth / 2.0f + m_rightSpace, 0.5f, 0.0f);
+        m_left.transform.position = new Vector3(-cameraWidth , 0.5f, 0.0f);
+        m_right.transform.position = new Vector3(cameraWidth , 0.5f, 0.0f);
 
-        m_top.transform.position = new Vector3(0.0f, 0.5f, cameraHeight / 2.0f + m_topSpace);
-        m_bot.transform.position = new Vector3(0.0f, 0.5f, -cameraHeight / 2.0f + m_botSpace);
+        m_top.transform.position = new Vector3(0.0f, 0.5f, m_camSize * m_topSpace);
+        m_bot.transform.position = new Vector3(0.0f, 0.5f, -m_camSize);
     }
 }
