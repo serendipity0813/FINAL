7c1ffaef90863de6686bdfd4628dc0a884322ef4
WooJin <106531904+h117562@users.noreply.github.com>
2024-01-26 10:47:09 +0900

    [Add] Move Wall Collider and Food Generate Range by device resolution

--- Assets/97.Test/NWJ/BasketGame.cs
+++ Assets/97.Test/NWJ/BasketGame.cs
@@ -10,6 +10,9 @@ public class BasketGame : MiniGameSetting
 
     [SerializeField] private GameObject m_player;//이동가능한 캐릭터
 
+    [SerializeField] private GameObject m_leftWall;//왼쪽 벽
+    [SerializeField] private GameObject m_rightWall;//오른쪽 벽
+
     private Rigidbody m_rigidbody;
 
     private int m_catchCounts = 0;//박스에 음식을 담은 개수
@@ -17,6 +20,7 @@ public class BasketGame : MiniGameSetting
     private int m_difficulty = 1;
     private Vector3 m_velocity;//캐릭터 이동 방향
     private float m_lastTime = 0.0f;//음식을 생성하고 지난 시간
+    private float m_screenWidth;//화면 해상도에 따른 오른쪽 끝의 World Point
 
     protected override void Awake()
     {
@@ -29,6 +33,18 @@ public class BasketGame : MiniGameSetting
         CameraManager.Instance.ChangeCamera(CameraView.ZeroView);//90도 각도로 내려다 보는 카메라로 변경
         m_rigidbody = m_player.GetComponent<Rigidbody>();
 
+        //화면 해상도 기준 오른쪽 끝에 레이 발사
+        Ray ray = CameraManager.Instance.GetCamera().ScreenPointToRay(new Vector3(Screen.width, Screen.height));
+        RaycastHit hit;
+
+        //좌우 벽을 화면 끝에 배치하는 기능
+        if (Physics.Raycast(ray, out hit))
+        {
+            m_screenWidth = hit.point.x - 0.2f;
+            m_leftWall.transform.position = new Vector3(-m_screenWidth, 0, 8.0f);
+            m_rightWall.transform.position = new Vector3(m_screenWidth, 0, 8.0f);
+        }
+        m_screenWidth -= 0.5f;//음식 생성 범위 조정 /음식이 벽 Collider에 튕기는 경우가 생겨서 벽보다 조금 안쪽에 생성되게
     }
 
     // Update is called once per frame
@@ -74,7 +90,7 @@ public class BasketGame : MiniGameSetting
     private void GenerateFoods()
     {
         int index = Random.Range(0, 3 + m_difficulty);//난이도만큼 쓰레기가 스폰될 확률 증가
-        Vector3 randomPos = new Vector3(Random.Range(-2.0f, 2.0f), 8.0f, 8.0f);//무작위 x 이동
+        Vector3 randomPos = new Vector3(Random.Range(-(m_screenWidth), (m_screenWidth)), 8.0f, 8.0f);//무작위 x 이동
         Quaternion randomRot = Quaternion.Euler(0.0f, Random.Range(0.0f, 180.0f), 0.0f);//무작위 y 회전
 
         GameObject food;
