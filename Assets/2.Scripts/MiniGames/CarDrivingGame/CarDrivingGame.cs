cb5552a6c0b0b9c5e009d76011eb937015d3555a
WooJin <106531904+h117562@users.noreply.github.com>
2024-01-31 11:53:51 +0900

    [Add] CarDriving Starts after 3 sec

--- Assets/2.Scripts/MiniGames/CarDrivingGame/CarDrivingGame.cs
+++ Assets/2.Scripts/MiniGames/CarDrivingGame/CarDrivingGame.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CarDrivingGame : MiniGameSetting
 {
-    [SerializeField] private GameObject m_player;
+    [SerializeField] private GameObject m_cameraTarget;
 
     private CharacterController m_playerController;
     private CarGenerator m_generator;
@@ -12,6 +10,7 @@ public class CarDrivingGame : MiniGameSetting
 
     private float m_runningSpeed = 0.4f;
     private float m_sideSpeed = 0.2f;
+    private float m_timer = 0.0f;
 
     protected override void Awake()
     {
@@ -21,12 +20,12 @@ public class CarDrivingGame : MiniGameSetting
     // Start is called before the first frame update
     private void Start()
     {
-        CameraManager.Instance.ChangeCamera(CameraView.ZeroView);
+        CameraManager.Instance.ChangeCamera(CameraView.Angle30View);
         CameraManager.Instance.SetFollowSpeed(10.0f);
-        CameraManager.Instance.SetFollowTarget(m_player);
+        CameraManager.Instance.SetFollowTarget(m_cameraTarget.transform.GetChild(0).gameObject);
         CameraManager.Instance.ToggleCameraFollow();
 
-        m_playerController = m_player.GetComponent<CharacterController>();
+        m_playerController = m_cameraTarget.GetComponent<CharacterController>();
         m_generator = transform.GetChild(2).GetComponent<CarGenerator>();
         m_generator.GenerateCars();//난이도에 맞춰서 차 생성
 
@@ -34,28 +33,32 @@ public class CarDrivingGame : MiniGameSetting
 
     private void FixedUpdate()
     {
+        m_timer += Time.deltaTime;
 
-        try
+        if (m_timer > 3.0f)//3초 지나고 시작
         {
-            m_playerController.Move(new Vector3(0, 0, m_runningSpeed));
-
-            if (TouchManager.instance.IsHolding())
+            try
             {
-                float direction = Input.mousePosition.x - ((float)Screen.width / 2);//화면을 절반으로 나누어 왼쪽, 오른쪽 부분 어디를 선택하였는지 체크
+                m_playerController.Move(new Vector3(0, 0, m_runningSpeed));
 
-                if (direction > 0)
+                if (TouchManager.instance.IsHolding())
                 {
-                    m_playerController.Move(new Vector3(m_sideSpeed, 0, 0));
-                }
-                else
-                {
-                    m_playerController.Move(new Vector3(-m_sideSpeed, 0, 0));
+                    float direction = Input.mousePosition.x - ((float)Screen.width / 2);//화면을 절반으로 나누어 왼쪽, 오른쪽 부분 어디를 선택하였는지 체크
+
+                    if (direction > 0)
+                    {
+                        m_playerController.Move(new Vector3(m_sideSpeed, 0, 0));
+                    }
+                    else
+                    {
+                        m_playerController.Move(new Vector3(-m_sideSpeed, 0, 0));
+                    }
                 }
             }
-        }
-        catch
-        {
-            Debug.Log("Player's Rigidbody is Null");
+            catch
+            {
+                Debug.Log("Player's Rigidbody is Null");
+            }
         }
 
     }
