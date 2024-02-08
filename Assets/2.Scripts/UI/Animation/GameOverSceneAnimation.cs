049bfe552929b1c8fb1743082b0d5144455c6154
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2024-02-08 11:46:32 +0900

    [Add] GameOver Scene Animation
    게임오버 씬 UI 애니메이션 작업 마무리 완료

--- Assets/2.Scripts/UI/Animation/GameOverSceneAnimation.cs
+++ Assets/2.Scripts/UI/Animation/GameOverSceneAnimation.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class GameOverSceneAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject[] Stars;
+    [SerializeField] GameObject[] StarsEffects;
+    [SerializeField] GameObject[] StarsBGEffects;
+    private int m_startNumber = 0;
+    private int m_startEffectNumer = 0;
+
+    public void StarEffect()
     {
-        
+        InvokeRepeating("EffectOn" , 0.1f, 0.1f);
+        InvokeRepeating("StarOn", 0.3f, 0.1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EffectOn()
     {
-        
+        StarsEffects[m_startEffectNumer].SetActive(true);
+        m_startEffectNumer++;
+        if (m_startEffectNumer == 5)
+            CancelInvoke("EffectOn");
     }
+
+    private void StarOn()
+    {
+        Stars[m_startNumber].SetActive(true);
+        StarsBGEffects[m_startNumber].SetActive(true);
+        m_startNumber++;
+        if (m_startNumber == 5)
+            CancelInvoke("StarOn");
+    }
+
 }
