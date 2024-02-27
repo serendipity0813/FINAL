ea8a2c8aa7ccb64d977e3183278c7d679682f249
newb <tydeo6029@naver.com>
2024-02-27 11:53:58 +0900

    [Change] ShopScene Coin
    상점 코인 지급
    테스트 기간에서 동작 될 예정

--- Assets/2.Scripts/UI/Controller/ShopUIController.cs
+++ Assets/2.Scripts/UI/Controller/ShopUIController.cs
@@ -100,7 +100,24 @@ public class ShopUIController : ButtonHandler
 
     public void BuyCoin()
     {
-        //PlayerDataManager.instance.GetCoin(m_itemCode);
+        switch(m_itemCode)
+        {
+            case 1000:
+                PlayerDataManager.instance.GetCoin(9000);
+                break;
+            case 1001:
+                PlayerDataManager.instance.GetCoin(30000);
+                break;
+            case 1002:
+                PlayerDataManager.instance.GetCoin(55000);
+                break;
+            case 1003:
+                PlayerDataManager.instance.GetCoin(120000);
+                break;
+            default:
+                Debug.LogWarning("코인 지급 예외처리 발생");
+                break;
+        }
         m_success.SetActive(true);
         EffectSoundManager.Instance.PlayEffect(2);
     }
