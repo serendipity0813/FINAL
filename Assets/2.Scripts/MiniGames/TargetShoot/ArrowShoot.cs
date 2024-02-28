6c80af818fd4a35953e810e39593640018e7dbdc
newb <tydeo6029@naver.com>
2024-02-28 16:34:43 +0900

    [Change] TargetShootGame
    1. 화살 날아가는 속도 증가 55 > 70
    2. 화살 길이 수정 Scale Z값 2 > 0.5
    3. 활 쏘는 버튼 Invoke 1초에서 2초로 증가
    4. 타겟 무브 속도 랜덤 값 + 속도 2 정도 감소 + 첫 시작 왼쪽 오른쪽 랜덤
    5. 난이도 수정 1 2 3 은 화살 개수, 1 4 7은 타겟 속도

--- Assets/2.Scripts/MiniGames/TargetShoot/ArrowShoot.cs
+++ Assets/2.Scripts/MiniGames/TargetShoot/ArrowShoot.cs
@@ -11,7 +11,7 @@ public class ArrowShoot : MonoBehaviour
         m_rb = GetComponent<Rigidbody>();
         targetShootGame = transform.GetComponentInParent<TargetShootGame>();
     }
-    private void Update()
+    private void FixedUpdate()
     {
         MoveArrow();
         if (m_isfly) // 날고 있을시
@@ -33,11 +33,13 @@ public class ArrowShoot : MonoBehaviour
             StickToTarget(collision.transform);
         }
     }
+
     // 타겟을 맞추면 해당 오브젝트가 맞춘 오브젝트 자식으로 들어간다.
     private void StickToTarget(Transform target)
     {
         transform.SetParent(target);
     }
+
     // 화살 이동 관련
     private void MoveArrow()
     {
@@ -52,16 +54,17 @@ public class ArrowShoot : MonoBehaviour
             m_rb.velocity = Vector3.zero; // 초기화
             m_isfly = false; // 초기화
             transform.rotation = Quaternion.Euler(-180f, 0f, 90f); // 회전을 멈추고 처음과 동일하게
-            transform.position = new Vector3(0f, 1.5f, -13f); //  m_rb.velocity를 멈춘뒤 포지션값을 처음과 동일하게
+            transform.position = new Vector3(0f, 1.7f, -11.75f); //  m_rb.velocity를 멈춘뒤 포지션값을 처음과 동일하게
         }
     }
+
     // 버튼에 연결할 메소드
     public void ShootArrow()
     {
         if (targetShootGame.m_arrowCount > 0)
         {
             m_isfly = true; // 날아가는 중
-            m_rb.velocity = new Vector3(0f, 0f, 55f); // 날아가는 힘
+            m_rb.velocity = new Vector3(0f, 0f, 70f); // 날아가는 힘
         }
     }
 }
