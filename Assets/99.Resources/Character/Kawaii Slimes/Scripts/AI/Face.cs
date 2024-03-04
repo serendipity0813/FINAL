bfcf04e0012b3ec2481285e5bf7ddcb5e5c55c7f
newb <tydeo6029@naver.com>
2024-03-04 20:48:01 +0900

    [Sort] Assets File
    안 쓰는 에셋 파일 삭제

--- Assets/99.Resources/Character/Kawaii Slimes/Scripts/AI/Face.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Face", order = 1)]
-public class Face :ScriptableObject
-{
-    public Texture Idleface, WalkFace, jumpFace, attackFace,damageFace;
-}
